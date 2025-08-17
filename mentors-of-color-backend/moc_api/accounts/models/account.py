99988b389256e92c0bb8f1dce349b74bd6c1076a
mudasirhayat <mudasirhayat5@gmail.com>
2025-08-17 09:00:06 +0000

    fix: improve error handling

--- mentors-of-color-backend/moc_api/accounts/models/account.py
+++ mentors-of-color-backend/moc_api/accounts/models/account.py
@@ -4,9 +4,9 @@ from django.db import models
 from moc_api.users.models import User
 
 class Account(TimestampMixin):
-    name = models.CharField(max_length=100)
-    account_owner_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account_owner')
-    unique_name = models.CharField(max_length=255, unique=True)
+name = models.CharField(max_length=100)
+account_owner_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account_owner')
+unique_name = models.CharField(max_length=255, unique=True)
     is_deleted = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
