0eaf6d87df0e7f1c21c7fd99ce1f509b5592b34b
mudasirhayat <mudasirhayat5@gmail.com>
2025-08-25 13:00:08 +0000

    refactor: improve code readability

--- mentors-of-color-frontend/src/pages/auth/register.js
+++ mentors-of-color-frontend/src/pages/auth/register.js
@@ -16,8 +16,8 @@ const Register = () => {
   return (
     <AuthWrapper>
       <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+<Grid item xs={12}>
+  <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
             <Typography variant="h3">Sign up</Typography>
             <Typography
               component={Link}
