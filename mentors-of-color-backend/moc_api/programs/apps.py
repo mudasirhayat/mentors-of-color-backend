7f2ff562f7044d7312d3bf2a1996068614320446
mudasirhayat <mudasirhayat5@gmail.com>
2025-09-02 14:56:18 +0000

    refactor: improve code readability

--- mentors-of-color-backend/moc_api/programs/apps.py
+++ mentors-of-color-backend/moc_api/programs/apps.py
@@ -5,5 +5,5 @@ class ProgramsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'moc_api.programs'
 
-    def ready(self):
-        import moc_api.programs.signals.handlers
+def ready(self):
+    import moc_api.programs.signals.handlers
