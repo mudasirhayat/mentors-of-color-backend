5b3913311c5ec4a8bafdafd419b1a6a358328860
mudasirhayat <mudasirhayat5@gmail.com>
2025-10-15 05:00:10 +0000

    fix: improve error handling

--- mentors-of-color-frontend/src/assets/images/profile/UserProfileBackLeft.js
+++ mentors-of-color-frontend/src/assets/images/profile/UserProfileBackLeft.js
@@ -11,10 +11,9 @@ const UserProfileBackLeft = () => {
       <path
         opacity="0.4"
         d="M-0.322477 0.641086L-0.418408 0.55164L-9.20939 59.4297L23.6588 106.206L154.575 130.423C236.759 117.931 383.93 93.3326 315.142 94.879C246.355 96.4253 215.362 64.2785 215.362 64.2785C215.362 64.2785 185.497 26.9117 117.864 33.4279C42.6115 40.6783 10.6143 10.8399 -0.322477 0.641086Z"
-        fill={alpha(theme.palette.primary.light, 0.4)}
-      />
-    </svg>
-  );
-};
-
+try {
+  fill = alpha(theme.palette.primary.light, 0.4);
+} catch (error) {
+  console.error('An error occurred while filling the color:', error);
+}
 export default UserProfileBackLeft;
