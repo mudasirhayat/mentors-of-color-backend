f887a9ec77d3ea024b1d0cbf9ba0c79f2f0b1dff
mudasirhayat <mudasirhayat5@gmail.com>
2025-10-28 05:00:08 +0000

    refactor: improve code readability

--- mentors-of-color-backend/moc_api/chat/models/chatmessagefile.py
+++ mentors-of-color-backend/moc_api/chat/models/chatmessagefile.py
@@ -5,5 +5,10 @@ from moc_api.users.models import User
 from .chatmessage import ChatMessage
 
 class ChatMessageFile(TimestampMixin):
-    chat_message_id = models.ForeignKey(ChatMessage, on_delete=models.CASCADE, related_name='files')
-    file = models.FileField(upload_to='chat_files/', blank=True, null=True)
+    chat_message_id = models.ForeignKey(
+        ChatMessage, 
+        on_delete=models.CASCADE, 
+        related_name='files'
+    )
+    file = models.FileField(
+        upload_to='chat_files/',
