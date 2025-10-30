429999e6b53aa374b5c08196ed4648ad152d71d0
mudasirhayat <mudasirhayat5@gmail.com>
2025-10-30 17:00:09 +0000

    refactor: improve code readability

--- mentors-of-color-backend/moc_api/users/migrations/0003_alter_historicaluserprofile_birth_date_and_more.py
+++ mentors-of-color-backend/moc_api/users/migrations/0003_alter_historicaluserprofile_birth_date_and_more.py
@@ -2,7 +2,6 @@
 
 from django.db import migrations, models
 
-
 class Migration(migrations.Migration):
     dependencies = [
         ('users', '0002_userprofile_historicaluser_last_activity_date_and_more'),
