029f15311aa871ce62bd7de31fbb91b4bd56f440
mudasirhayat <mudasirhayat5@gmail.com>
2025-10-31 17:00:09 +0000

    refactor: improve code readability

--- mentors-of-color-frontend/src/components/Loader.js
+++ mentors-of-color-frontend/src/components/Loader.js
@@ -5,9 +5,10 @@ import LinearProgress from '@mui/material/LinearProgress';
 // loader style
 const LoaderWrapper = styled('div')(({ theme }) => ({
   position: 'fixed',
-  top: 0,
-  left: 0,
   zIndex: 2001,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   width: '100%',
   '& > * + *': {
     marginTop: theme.spacing(2)
