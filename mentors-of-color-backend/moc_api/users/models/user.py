5b869b5088bd55f33a43dc36420024d9cab8c8a7
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-11 09:00:08 +0000

    fix: improve error handling

--- mentors-of-color-backend/moc_api/users/models/user.py
+++ mentors-of-color-backend/moc_api/users/models/user.py
@@ -67,9 +67,9 @@ class UserProfile(TimestampMixin):
         try
     )
     first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    phone = models.CharField(max_length=16, null=True, blank=True)
-    username = models.CharField(max_length=30)
+last_name = models.CharField(max_length=30)
+phone = models.CharField(max_length=16, null=True, blank=True)
+username = models.CharField(max_length=30)
     profile_photo_url = models.CharField(max_length = 200, null=True, blank=True)
     online_status = models.BooleanField(default=False)
     birth_date = models.DateField(null=True)
