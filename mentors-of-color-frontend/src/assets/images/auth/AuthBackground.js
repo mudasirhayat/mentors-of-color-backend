ac599c44720d0ce5949f9246d5b77972d6cee388
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-25 09:00:06 +0000

    refactor: improve code readability

--- mentors-of-color-frontend/src/assets/images/auth/AuthBackground.js
+++ mentors-of-color-frontend/src/assets/images/auth/AuthBackground.js
@@ -8,12 +8,11 @@ import { ThemeDirection, ThemeMode } from 'config';
 // ==============================|| AUTH BLUR BACK SVG ||============================== //
 
 const AuthBackground = () => {
-  const theme = useTheme();
-  return (
-    <Box
-      sx={{
-        position: 'absolute',
-        filter: 'blur(18px)',
+const theme = useTheme();
+const styles = {
+  position: 'absolute',
+  filter: 'blur(18px)',
+};
         zIndex: -1,
         bottom: 0,
         transform: theme.direction === ThemeDirection.RTL ? 'rotate(180deg)' : 'inherit'
