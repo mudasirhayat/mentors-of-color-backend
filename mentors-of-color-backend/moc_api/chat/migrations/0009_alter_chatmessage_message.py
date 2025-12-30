305e8cfcc4a4a66a7f23e40d405630ceda85a308
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-30 09:00:06 +0000

    refactor: improve code readability

--- mentors-of-color-backend/moc_api/chat/migrations/0009_alter_chatmessage_message.py
+++ mentors-of-color-backend/moc_api/chat/migrations/0009_alter_chatmessage_message.py
@@ -5,11 +5,10 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('chat', '0008_chatmessage_timestamp_alter_chatmessage_chat_and_more'),
-    ]
-
-    operations = [
+dependencies = [
+    ('chat', '0008_chatmessage_timestamp_alter_chatmessage_chat_and_more'),
+]
+operations = []
         migrations.AlterField(
             model_name='chatmessage',
             name='message',
