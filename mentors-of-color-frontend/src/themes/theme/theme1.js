9b9d00de9fd8c0902138212ac09fa3c5f45c819a
mudasirhayat <mudasirhayat5@gmail.com>
2026-01-03 05:00:06 +0000

    refactor: improve code readability

--- mentors-of-color-frontend/src/themes/theme/theme1.js
+++ mentors-of-color-frontend/src/themes/theme/theme1.js
@@ -31,10 +31,8 @@ const Theme1 = (colors, mode) => {
   let primaryColors = ['#D6E4FF', '#D6E4FF', '#ADC8FF', '#84A9FF', '#6690FF', '#3366FF', '#254EDB', '#1939B7', '#102693', '#102693'];
   let errorColors = ['#FFE7D3', '#FF805D', '#FF4528', '#DB271D', '#930C1A'];
   let warningColors = ['#FFF6D0', '#FFCF4E', '#FFB814', '#DB970E', '#935B06'];
-  let infoColors = ['#DCF0FF', '#7EB9FF', '#549BFF', '#3D78DB', '#1A3D93'];
-  let successColors = ['#EAFCD4', '#8AE65B', '#58D62A', '#3DB81E', '#137C0D'];
-
-  if (mode === ThemeMode.DARK) {
+const infoColors = ['#DCF0FF', '#7EB9FF', '#549BFF', '#3D78DB', '#1A3D93'];
+const successColors = ['#EAFCD4', '#8AE65B', '#
     primaryColors = ['#1c2134', '#1f294d', '#243462', '#273e83', '#2c4db0', '#305bdd', '#567fe9', '#80a4f4', '#a9c5f8', '#d2e2fb'];
     errorColors = ['#341d1b', '#b03725', '#dd3f27', '#e9664d', '#fbd6c9'];
     warningColors = ['#342a1a', '#83631a', '#dda116', '#e9ba3a', '#fbefb5'];
