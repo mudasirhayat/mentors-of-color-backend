5eb06afe97f7d3ede96a9ba7c87dbc0196b2874f
mudasirhayat <mudasirhayat5@gmail.com>
2026-01-14 17:00:10 +0000

    refactor: improve code readability

--- mentors-of-color-backend/moc_api/users/apps.py
+++ mentors-of-color-backend/moc_api/users/apps.py
@@ -2,8 +2,8 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'moc_api.users'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APP_NAME = 'moc_api.users'
 
     def ready(self):
         import moc_api.users.signals.handlers
