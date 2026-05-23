59ff33a5ad48d8fcb87ead8824062debc299961d
mudasirhayat <mudasirhayat5@gmail.com>
2026-05-23 13:00:09 +0000

    refactor: improve code readability

--- mentors-of-color-frontend/src/pages/auth/reset-password.js
+++ mentors-of-color-frontend/src/pages/auth/reset-password.js
@@ -12,8 +12,12 @@ const ResetPassword = () => (
     <Grid container spacing={3}>
       <Grid item xs={12}>
         <Stack sx={{ mb: { xs: -0.5, sm: 0.5 } }} spacing={1}>
-          <Typography variant="h3">Reset Password</Typography>
-          <Typography color="secondary">Please choose your new password</Typography>
+<Typography variant="h3">
+  Reset Password
+</Typography>
+<Typography color="secondary">
+  Please choose your new password
+</Typography>
         </Stack>
       </Grid>
       <Grid item xs={12}>
