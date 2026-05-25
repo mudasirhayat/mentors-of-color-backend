7ec504a5441ffc76a0eeb1dac42cbfa91eb0ad59
mudasirhayat <mudasirhayat5@gmail.com>
2026-05-25 05:00:08 +0000

    refactor: improve code readability

--- mentors-of-color-frontend/src/menu-items/index.js
+++ mentors-of-color-frontend/src/menu-items/index.js
@@ -1,9 +1,9 @@
 // project import
 import applications from './applications';
 import dashboardCollapse from './dashboardCollapse';
-import widget from './widget';
-import formsTables from './forms-tables';
-import chartsMap from './charts-map';
+import Widget from './widget';
+import FormsTables from './forms-tables';
+import ChartsMap from './charts-map';
 import samplePage from './sample-page';
 import other from './other';
 import pages from './pages';
