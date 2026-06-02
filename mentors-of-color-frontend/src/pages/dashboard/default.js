bca92e9a226b68899ef9f6b766842ab330019313
mudasirhayat <mudasirhayat5@gmail.com>
2026-06-02 05:00:07 +0000

    fix: improve error handling

--- mentors-of-color-frontend/src/pages/dashboard/default.js
+++ mentors-of-color-frontend/src/pages/dashboard/default.js
@@ -85,10 +85,11 @@ const DashboardDefault = () => {
       <Grid item xs={12} sm={6} md={4} lg={3}>
         <AnalyticEcommerce title="Total Users" count="78,250" percentage={70.5} extra="8,900" />
       </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Total Order" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
+<Grid item xs={12} sm={6} md={4} lg={3}>
+  {error ? (
+    <ErrorComponent message="Failed to load data" />
+  ) : (
+    <AnalyticEcommerce title
         <AnalyticEcommerce title="Total Sales" count="$35,078" percentage={27.4} isLoss color="warning" extra="$20,395" />
       </Grid>
 
