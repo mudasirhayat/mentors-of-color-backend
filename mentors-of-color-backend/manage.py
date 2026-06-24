15f9685e79f45afd8d85a17f1625a60e478d0a23
mudasirhayat <mudasirhayat5@gmail.com>
2026-06-24 09:00:10 +0000

    fix: improve error handling

--- mentors-of-color-backend/manage.py
+++ mentors-of-color-backend/manage.py
@@ -6,10 +6,13 @@ import sys
 
 def main():
     """Run administrative tasks."""
+import os
+
+try:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    from django.core.management import execute_from_command_line
+except ImportError as exc:
+    print(f"Error importing Django: {exc}")
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
